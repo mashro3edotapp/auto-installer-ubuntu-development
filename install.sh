c4ca4f75d500e095171a4b738b04b78b0896ab79
mashro3edotapp <mashro3edotapp@gmail.com>
2023-05-11 17:17:36 +0000

    Initial commit

---
+++ install.sh
@@ -0,0 +1,50 @@
+﻿
+
+cd ~ && sudo apt-get update
+
+
+
+git config --global user.name "mashro3edotapp"
+git config --global user.email mashro3edotapp@gmail.com
+
+
+
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.7.12-stable.tar.xz
+
+tar xf ./flutter_linux_3.7.12-stable.tar.xz
+
+export PATH="$PATH:`pwd`/flutter/bin"
+
+flutter precache
+
+
+
+
+
+echo 'Installing VSCode'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https -y
+sudo apt-get update && sudo apt-get install code -y
+
+
+
+
+echo 'Installing Android Studio'
+sudo add-apt-repository ppa:maarten-fonville/android-studio -y
+sudo apt-get update && sudo apt-get install android-studio -y
+
+
+echo 'Enabling KVM for Android Studio'
+sudo apt-get install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager -y
+sudo adduser $USER libvirt
+sudo adduser $USER libvirt-qemu
+
+
+
+
+echo 'Updating and Cleaning Unnecessary Packages'
+sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get full-upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
+clear
+echo 'All setup, enjoy!'
